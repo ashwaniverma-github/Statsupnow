ea5f9091c7c1acb06f7cdf6ebd3165ce7814e85f
ashwaniverma-github <ap8606574@gmail.com>
2024-08-07 17:11:03 +0530

    navbar fixed

--- src/components/userApp.tsx
+++ src/components/userApp.tsx
@@ -1,13 +1,6 @@
 'use client'
 import { useState } from "react";
-import {
-  Home,
-  LineChart,
-  LineChartIcon,
-  Menu,
-  Settings2,
-  Youtube,
-} from "lucide-react";
+import { Home, LineChart, Menu, Settings2, Youtube } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Preference from "@/pages/preference"; 
@@ -21,12 +14,12 @@ export default function UserApp() {
     switch (activeComponent) {
       case "Preferences":
         return <Preference />;
-      case "SubscribedChannels":
-        return <SubscribedChannels/>;
+      case "Subscribed Channels":
+        return <SubscribedChannels />;
       case "Stats":
-        return <Stats/>
+        return <Stats />;
       default:
-        return <Preference/>
+        return <Preference />;
     }
   };
 
@@ -45,7 +38,7 @@ export default function UserApp() {
       <div className="hidden border-r border-red-300 md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center px-4 lg:h-[60px] lg:px-6"></div>
-            <div className="flex-1  ">
+            <div className="flex-1">
               <nav className="grid items-start shadow-slate-100 px-2 text-sm font-medium lg:px-4 fixed">
                 <button
                   onClick={() => setActiveComponent("Preferences")}
@@ -56,25 +49,25 @@ export default function UserApp() {
                 </button>
 
                 <button
-                  onClick={() => setActiveComponent("SubscribedChannels")}
-                  className={getNavButtonClass("SubscribedChannels")}
+                  onClick={() => setActiveComponent("Subscribed Channels")}
+                  className={getNavButtonClass("Subscribed Channels")}
                 >
                   <Youtube className="h-4 w-4" />
-                  Subscribed channels {getNavArrow("SubscribedChannels")}
+                  Subscribed channels {getNavArrow("Subscribed Channels")}
                 </button>
 
                 <button
                   onClick={() => setActiveComponent("Stats")}
                   className={getNavButtonClass("Stats")}
                 >
-                  <LineChartIcon className="h-4 w-4" />
+                  <LineChart className="h-4 w-4" />
                   Stats {getNavArrow("Stats")}
                 </button>
               </nav>
             </div>
         </div>
       </div>
-      <div className="flex flex-col min-h-screen ">
+      <div className="flex flex-col min-h-screen">
         <header className="flex h-14 items-center gap-4 border-b px-4 lg:h-[60px] lg:px-6">
           <Sheet>
             <SheetTrigger asChild>
@@ -84,7 +77,7 @@ export default function UserApp() {
               </Button>
             </SheetTrigger>
             <SheetContent side="left" className="flex flex-col bg-black">
-              <nav className="grid gap-2 text-lg font-medium ">
+              <nav className="grid gap-2 text-lg font-medium">
                 <button
                   onClick={() => setActiveComponent("Preferences")}
                   className={getNavButtonClass("Preferences")}
@@ -93,17 +86,17 @@ export default function UserApp() {
                   Preferences {getNavArrow("Preferences")}
                 </button>
                 <button
-                  onClick={() => setActiveComponent("Subscribed channels")}
-                  className={getNavButtonClass("Subscribed channels")}
+                  onClick={() => setActiveComponent("Subscribed Channels")}
+                  className={getNavButtonClass("Subscribed Channels")}
                 >
                   <LineChart className="h-5 w-5" />
-                  Subscribed channels {getNavArrow("Subscribed channels")}
+                  Subscribed channels {getNavArrow("Subscribed Channels")}
                 </button>
                 <button
                   onClick={() => setActiveComponent("Stats")}
                   className={getNavButtonClass("Stats")}
                 >
-                  <LineChartIcon className="h-4 w-4" />
+                  <LineChart className="h-4 w-4" />
                   Stats {getNavArrow("Stats")}
                 </button>
               </nav>
